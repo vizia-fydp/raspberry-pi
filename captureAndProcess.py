66be740bea47a1f79302c571c7f7c00560a831b5
Zain Denno <zdbrain@gmail.com>
2021-08-01 21:14:34 +0100

    captured first image

--- captureAndProcess.py
+++ captureAndProcess.py
@@ -1,1 +1,10 @@
-# Hello sir
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1024,768)
+camera.start_preview()
+
+sleep(2)
+camera.capture('test.jpg')
+
