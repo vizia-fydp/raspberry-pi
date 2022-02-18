a4bb1b357fc3e1d3573a81141fa09241c65d1acf
Waleed Ahmed <waleed@wahmed.dev>
2022-02-18 23:40:50 +0000

    works now

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ GPIO.setmode(GPIO.BCM)
 
 # Setup buttons as inputs. GPIO.PUD_UP means use internal pull up resister.
 # Button is HIGH while not pressed, and LOW while pressed
-GPIO.setup(BUTTON1_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(BUTTON2_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(BUTTON1_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(BUTTON2_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 # Setup interrupts on rising edge (button released)
-GPIO.add_event_detect(BUTTON1_PIN, GPIO.RISING, callback = button1,
+GPIO.add_event_detect(BUTTON1_PIN, GPIO.FALLING, callback = button1,
     bouncetime = BUTTON_BOUNCE_MS)
-GPIO.add_event_detect(BUTTON2_PIN, GPIO.RISING, callback = button2,
+GPIO.add_event_detect(BUTTON2_PIN, GPIO.FALLING, callback = button2,
     bouncetime = BUTTON_BOUNCE_MS)
 
 ######### Event loop to keep the program running #########
